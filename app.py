4a16cc3daf3d72a1bb2ee2f61e4732757a04ed3e
R. Kyle Kennedy <r.kyle.kennedy@gmail.com>
2026-07-12 15:24:29 -0700

    added google login and requirements.txt

--- app.py
+++ app.py
@@ -1,32 +1,153 @@
-from flask import (render_template)
+import json
+import os
+import sqlite3
+from flask import Flask, redirect, request, url_for, render_template
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
+from oauthlib.oauth2 import WebApplicationClient
+import requests
 from flask_bootstrap import Bootstrap
 from models import db, app, User
+from dotenv import load_dotenv
 
+load_dotenv()
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+
+app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
+login_manager = LoginManager()
+login_manager.init_app(app)
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+# id = 1
+# def get_user():
+#     # TODO: try to find user and set id else show default info with id = 1
+#     pass
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if current_user.is_authenticated:
+        return (
+            "<p>Hello, {}! You're logged in! Email: {}</p>"
+            "<div><p>Google Profile Picture:</p>"
+            '<img src="{}" alt="Google profile pic"></img></div>'
+            '<a class="button" href="/logout">Logout</a>'.format(
+                current_user.name, current_user.email, current_user.profile_pic
+            )
+        )
+    else:
+        return '<a class="button" href="/login">Google Login</a>'
+    # return render_template('index.html')
+
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
+
+@app.route("/login")
+def login():
+    # Find out what URL to hit for Google login
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+    # Use library to construct the request for Google login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
+
+@app.route('/login/callback')
+def callback():
+    # Get authorization code Google sent back to you
+    code = request.args.get("code")
+
+    # Find out what URL to hit to get tokens that allow you to ask for
+    # things on behalf of a user
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+
+    # Parse the tokens!
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+
+    if userinfo_response.json().get("email_verified"):
+        unique_id = userinfo_response.json()["sub"]
+        users_email = userinfo_response.json()["email"]
+        picture = userinfo_response.json()["picture"]
+        users_name = userinfo_response.json()["given_name"]
+    else:
+        return "User email not available or not verified by Google.", 400
+
+
+    user = User(
+        id=unique_id, name=users_name, email=users_email, profile_image_url=picture
+    )
+
+    # Doesn't exist? Add it to the database.
+    if not User.get(unique_id):
+        User.create(unique_id, users_name, users_email, picture)
+
+    # Begin user session by logging the user in
+    login_user(user)
+
+    # Send user back to homepage
+    return redirect(url_for('index'))
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/new')
 def new_recipe():
     return render_template('new_recipe.html')
 
-@app.route('/recipes/<id>')
-def recipe_book(id):
-    user = User.query.get_or_404(id)
-    # recipes = user['Recipes']
-    return render_template('recipe_book.html', user=user)
+@app.route('/recipes')
+def recipe_book():
+    recipes = User.query.get_or_404(id).recipes
+    return render_template('recipe_book.html', recipes=recipes)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+# @app.route('/login')
+# def login():
+#     return render_template('login.html')
 
 if __name__=='__main__':
     Bootstrap(app)
     with app.app_context():
         db.create_all()
+    # get_user()
     app.run(debug=True)
--- models.py
+++ models.py
@@ -9,25 +9,13 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column('Name', db.String())
+    email = db.Column('Email', db.String())
+    profile_image_url = db.Column('ProfileImageUrl', db.String())
     recipes = db.Column('Recipes', db.JSON())
 
     def __repr__(self):
         return f'''User (name: {self.name}
                 recipes: {self.recipes}
-                id: {self.id}'''
-
-# class Recipe(db.Model):
-#     id = db.mapped_column(db.Integer, primary_key=True)
-#     user_id = db.Column(db.Integer)
-#     name = db.Column('Name', db.string())
-#
-#     def __repr__(self):
-#         return f'''Recipe (name: {self.name}
-#                 id: {self.id}'''
-#
-#
-# class Ingrediant(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     recipe_id = db.Column(db.Integer)
-#     name = db.Column('Name', db.string())
-#     url = db.Column('Url', db.String())
+                id: {self.id}
+                email: {self.email}
+                profile_image_url: {self.profile_image_url}'''
